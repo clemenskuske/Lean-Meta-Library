23502694f6b7cc2b29e2ae13acaef417ca61d2fa
clemenskuske <33251410+clemenskuske@users.noreply.github.com>
2026-06-02 13:55:08 +0200

    Clean submissions and remove test package

--- TestPaper-package/surface-package/ConnectedGraph/Surface.lean
+++
@@ -1,8 +0,0 @@
-import Mathlib.Combinatorics.SimpleGraph.Connectivity.Connected
-
-namespace TestPaper.Surface.Definition.ConnectedGraph
-
-def IsConnectedGraph {V : Type u} (G : SimpleGraph V) : Prop :=
-  G.Connected
-
-end TestPaper.Surface.Definition.ConnectedGraph
